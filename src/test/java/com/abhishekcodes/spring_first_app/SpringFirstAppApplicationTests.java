7a562a742ec1081faddf2bc98eb41a8d32d0a252
abhisheksharma5798 <abhishekanand5798@gmail.com>
2024-05-24 03:23:07 +0530

    First Springboot Project

---
+++ src/test/java/com/abhishekcodes/spring_first_app/SpringFirstAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abhishekcodes.spring_first_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFirstAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
